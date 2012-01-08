21af46f83d0289fc3d5043d298a2e9f5b801e000
Reginald Pierce <pierce.reginald@gmail.com>
2012-01-08 13:39:06 -0500

    Created a views row plugin to display rows in collapsible fieldsets

---
+++ views_fieldset/views-view-row-fieldset.tpl.php
@@ -0,0 +1,31 @@
+<?php
+//Created by Reginald Pierce, RIT Honors Tech Committee
+$first = true;
+?>
+<fieldset class="collapsible">
+<?php foreach ($fields as $id => $field): ?>
+  <?php if($first): //Make the first one a legend
+           $first = false; ?>
+    <legend>
+      <?php print $field->content; ?>
+    </legend>
+  <?php else: ?>
+	  <?php if (!empty($field->separator)): ?>
+		<?php print $field->separator; ?>
+	  <?php endif; ?>
+
+	  <<?php print $field->inline_html;?> class="views-field-<?php print $field->class; ?>">
+		<?php if ($field->label): ?>
+		  <label class="views-label-<?php print $field->class; ?>">
+			<?php print $field->label; ?>:
+		  </label>
+		<?php endif; ?>
+		  <?php
+		  // $field->element_type is either SPAN or DIV depending upon whether or not
+		  // the field is a 'block' element type or 'inline' element type.
+		  ?>
+		  <<?php print $field->element_type; ?> class="field-content"><?php print $field->content; ?></<?php print $field->element_type; ?>>
+	  </<?php print $field->inline_html;?>>
+  <?php endif; ?>
+<?php endforeach; ?>
+</fieldset>
