f0c98e31c13fa96c76694d7eda5ced09828b15c2
Reggie Pierce <pierce.reginald@gmail.com>
2012-12-18 23:08:36 -0500

    Fixed checkcomments

--- checkcomments.sh
+++ checkcomments.sh
@@ -2,10 +2,10 @@
 Out="./annotations.textile"
 echo -e "h1. Annotations" > "${Out}"
 
-for f in `grep -Rl \/\/[A-Z]*:`
+for f in `grep -Rl --exclude="annotations*" \/\/[A-Z]*:`
 do
-	echo -e "h2. $f" >> "${Out}"
-	echo -e "\`\`\`php" >> "${Out}"
+	echo -e "\nh2. $f\n" >> "${Out}"
+	echo -e "\`\`\`" >> "${Out}"
 	grep -nT -A 3 \/\/[A-Z]*: $f >> "${Out}"
 	echo -e "\`\`\`" >> "${Out}"
 done
