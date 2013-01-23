1d96ca7a6a781664b8bfe3f31f5b781213b2085b
Reggie Pierce <pierce.reginald@gmail.com>
2013-01-22 21:07:34 -0500

    started bulkui

---
+++ hdata/hpoints/bulkui.tpl.php
@@ -0,0 +1,26 @@
+<?php
+$closelink = '<a class="del"href="#"><img src="'.base_path().drupal_get_path('module','hpoints').'/close.png"" width=16 height=16 ></a>';
+?>
+<!-- Template for Production Code -->
+<div class="hpoints-bulkui-container">
+<!--<span class="input parent">rdp2575 <?php print $closelink ?></span>
+<span class="input parent">rdp2576 <?php print $closelink ?></span>
+<span class="input parent">rdp2577 <?php print $closelink ?></span>
+<span class="input parent">rdp2578 <?php print $closelink ?></span>
+<span class="input parent">rdp2579 <?php print $closelink ?></span>
+<span class="input parent">rdp2580 <?php print $closelink ?></span>
+<div class="group parent">
+<span class="group group-label">KGCOE <?php print $closelink ?></span>
+<span class="input parent">rdp2512 <?php print $closelink ?></span>
+<span class="input parent">rdp2513 <?php print $closelink ?></span>
+<span class="input parent">rdp2514 <?php print $closelink ?></span>
+<span class="input parent">rdp2515 <?php print $closelink ?></span>
+<span class="input parent">rdp2516 <?php print $closelink ?></span>
+<span class="input parent">rdp2517 <?php print $closelink ?></span>
+</div>
+<span class="input parent">rdp2500 <?php print $closelink ?></span>-->
+</div>
+<textarea rows=4 cols=50 class=hpoints-bulkui-textarea >jdk3414 rdp2575 rdprap dtsgla txt1234 brg6453 bsc4900=[KGCOE] rdp2575</textarea>
+<!-- Testing Only -->
+<div class="hpoints-bulkui-test"></div>
+<input type=button class=test-button value=Test>
