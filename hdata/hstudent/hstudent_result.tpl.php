5dc70abd745811f4871c1391e251bbc4db0625c5
Connor D. Pierce <conpierce8@gmail.com>
2013-02-27 13:42:48 -0500

    Started testing
    Shifted display of search results to a template; fixed info file;
    attemping to make the module work

---
+++ hdata/hstudent/hstudent_result.tpl.php
@@ -0,0 +1,7 @@
+<h1>
+	<a href='<?php print $base_path.'/userhistory/'.$username;?>'>
+		<?php print $fname.' '.$lname.' ('.$username.')';?>
+	</a>
+</h1>
+<div><?php print $year;?></div>
+<div><?php print $major;?></div>
