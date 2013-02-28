cc4f8ef89e829236d999f55b4524d6a72cc6a3fb
ben <bsc9718@rit.edu>
2013-02-27 20:50:43 -0500

    other points currently not working

--- hdata/hstatus/mystatus.tpl.php
+++ hdata/hstatus/mystatus.tpl.php
@@ -23,16 +23,46 @@
  * $year - user's year level
  * $courses - array of courses taken
  *    - each element is an array with keys 'num','name','term','grade','points','instructor'
+ * $points username, explanation,
+ *  	pointID as type,
+ * 		submitted as submitted_time,
+		submittedBy as submitted_by,
+		approved as status,
+		approvedBy as reviewer,
+		approvedTime as reviewed_time,
+		declinedReason as reason,
+		numPoints as credits
  * 
  * TODO
  * -figure out how "other points" will work
  * -figure out how complearning submissions will work
  */
  
+
 if($displaynone):
 ?>
 <h2>You are not in the Honors Database</h2>
 <?php else: ?>
+<h2>Continuation Status </h2>
+ <p>Your continuation status is 
+<?php
+//provide a background color for select statuses
+switch($status){
+    case 0: $color = '#A2EB81';break;//green
+    case 1: $color = '#D0EB81';break;//green-yellow
+    case 2: $color = '#E7EB81';break;//yellow
+    case 3: $color = '#EB8181';break;//red
+}
+//print opening tag  
+if(isset($color)){
+    print "<span style=\"background-color:$color\" >";
+}
+?>
+
+<strong><?php print $status_string; ?></strong>
+
+<?php if(isset($color)){ print '</span>'; }/*print closing tag.*/ ?>
+</p>
 
 <h2><?php t('Honor Status for @user',array('@user'=>$username)); ?></h2>
 <table>
@@ -80,28 +110,54 @@ foreach($courses as $value)
 $html_table.='</tbody></table>';
 echo $html_table;
 ?>
+<h3>Other Activities</h3>
+<?php
+$num_elm=count($points);
+$html_table='<table>
+			<thead>
+				<tr><td>Explanation</td>
+				<td>Status</td>
+				<td>Points</td>
+				<td>Date Submitted</td>
+				</tr>
+			</thead>
+			<tbody>';
+foreach($points as $value)
+{	
+	$html_table .='<tr><td>'.$value['explanation']. '</td>
+					<td>' .$value['approved'] .'</td>
+					<td>' .$value['numPoints'] .'</td>
+					<td>' .$value['submittedTime'] .'</td>
+					 </tr>';				
+}
+$html_table.='</tbody></table>';
+echo $html_table;
+?>
+<h3>Point Request Form</h3>
 
+
+<h2>Comp Learning Submissions</h2>
+<p>For requirements, please see the <a href="http://www-staging.rit.edu/academicaffairs/honors/service-requirements">comp learning page</a>.</p>
 <p>Your complearning has been <strong><?php print $complearning_status_string; ?></strong></p>
 
-<p>Your continuation status is 
-<?php
-//provide a background color for select statuses
-switch($status){
-    case 0: $color = '#A2EB81';break;//green
-    case 1: $color = '#D0EB81';break;//green-yellow
-    case 2: $color = '#E7EB81';break;//yellow
-    case 3: $color = '#EB8181';break;//red
-}
-//print opening tag  
-if(isset($color)){
-    print "<span style=\"background-color:$color\" >";
-}
-?>
 
-<strong><?php print $status_string; ?></strong>
+<h2>Questions?</h2>
 
-<?php if(isset($color)){ print '</span>'; }/*print closing tag.*/ ?>
-</p>
+<strong>Continuation Review</strong>
+<p>Email any questions to <a href="mailto:honors@rit.edu">
+honors@rit.edu</a></p>
+
+<strong>Comp Learning</strong>
+<p><a href="http://www-staging.rit.edu/academicaffairs/honors/service-requirements">info about comp learning requirements</a></p>
+<p>Email any questions to <a href="mailto:CompLearning@mail.honors.rit.edu">CompLearning@mail.honors.rit.edu</a></p>
+
+<strong>General Requirements</strong>
+<p><a href=http://www-staging.rit.edu/academicaffairs/honors/academic-requirements>info about honors requirements</a></p>
+<p>Email any questions to <a href="mailto:Council@mail.honors.rit.edu">Council@mail.honors.rit.edu</a></p>
+
+<strong>Honors Courses</strong>
+<p><a href="http://www-staging.rit.edu/academicaffairs/honors/courseslist">A Listing of the currently offered honors courses</a></p>
+
 <pre>
 <?php print_r($huser); /*DEBUG*/ ?>
 </pre>
