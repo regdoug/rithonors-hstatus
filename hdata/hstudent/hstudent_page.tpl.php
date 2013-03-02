47d4ea5e983e1650fd23db54bd0c73c8842303d2
Reggie Pierce <pierce.reginald@gmail.com>
2013-03-01 19:26:26 -0500

    Added skeleton student history page
    Fixes #31 by adding a student history page skeleton.  Only the
    basic info is in place, but that is good enough for the alpha2
    release.

--- hdata/hstudent/hstudent_page.tpl.php
+++ hdata/hstudent/hstudent_page.tpl.php
@@ -1,2 +1,48 @@
 <h1>Student History</h1>
-<p>Under Construction</p>
+<h2>Personal Information</h2>
+<section>
+<div style="display:inline-block; width:75%">
+<table <?php if($status != 1) print 'style="background-color:#eee;"';?>>
+    <tr>
+        <td>Name</td>
+        <td><?php print "$fname $lname"; ?></td>
+    </tr>
+    <tr>
+        <td>Status</td>
+        <td><?php print "$status_string"; ?></td>
+    </tr>
+    <tr>
+        <td>Major</td>
+        <td><?php print "$year $major ";//.(empty($plan))?"":"($plan $subplan)";?></td>
+    </tr>
+    <tr>
+        <td>Entered RIT</td>
+        <td><?php print "$enteredrit"; ?></td>
+    </tr>
+    <tr>
+        <td>Entered Honors</td>
+        <td><?php print "$enteredhonors"; ?></td>
+    </tr>
+    <tr>
+        <td>Left Honors</td>
+        <td><?php print "$lefthonors"; ?></td>
+    </tr>
+</table>
+</div>
+<div style="float:right;">
+<img src="<?php print base_path()."facebook/portraits/$username.jpg";?>" width=150 height=200 alt="<?php print $username; ?>" />
+</div>
+</section>
+<h2>Administrative Tasks</h2>
+<p>Future content will include
+<ul>
+    <li>Change Status</li>
+    <li>Grant Waivers</li>
+    <li>Continuation?</li>
+</ul></p>
+<h2>Continuation Information</h2>
+<p>Continuation system needs overhaul before this will be functional</p>
+<h2>Comp Learning, Courses, and Points</h2>
+<p>Same as MyStatus page... use theme functions?</p>
+<h2>Debugging</h2>
+<pre><?php print $vardump; ?></pre>
--- hdata/hstudent/hstudent_result.tpl.php
+++ hdata/hstudent/hstudent_result.tpl.php
@@ -1,7 +1,5 @@
 <h1>
-	<a href='<?php print $base_path.'/student_history/'.$username;?>'>
-		<?php print $fname.' '.$lname.' ('.$username.')';?>
-	</a>
+	<?php print l($fname.' '.$lname.' ('.$username.')','student_history/'.$username);?>
 </h1>
 <div><?php print $year;?></div>
 <div><?php print $major;?></div>
